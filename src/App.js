0cfaa4635a542f2627d152d7c1b9e8b5a77be535
Bogdan Tabor <bogdan.tabor@gmail.com>
2021-09-18 10:05:46 +0300

    remake App component

--- src/App.js
+++ src/App.js
@@ -1,62 +1,49 @@
-import './App.css';
+import React from "react";
 
-import React, { Component } from 'react'
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-class Table extends Component {
-   constructor(props) {
-      super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
-      this.state = { //state is by default an object
-         students: [
-            { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com' },
-            { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
-            { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
-            { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' }
-         ]
-      }
-   }
-
-  componentDidMount() {
-    const apiUrl = 'https://itrex-react-lab-files.s3.eu-central-1.amazonaws.com/react-test-api.json';
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    this.state = {
+      users: [],
+    };
   }
+
+  async getUsersData() {
+    const res = await fetch('https://itrex-react-lab-files.s3.eu-central-1.amazonaws.com/react-test-api.json');
+    const data = await res.json();
+    return data.results;
+  }
+
+  async componentDidMount() {
+    const users = await this.getUsersData();
+    this.setState({ users });
+  }
+
+
   
+
   render() {
+    const User = ({ id, firstName }) => (
+      <div>
+        <p>{id}</p>
+        <p>{firstName}</p>
+      </div>
+    );
+    
     return (
       <div>
-        <h1>Component has Mounted</h1>
-        <h1 id='title'>React Dynamic Table</h1>
-        <table id='students'>
-            <tbody>
-              <tr>{this.renderTableHeader()}</tr>
-              {this.renderTableData()}
-            </tbody>
-        </table>
+        {this.state.users.map((user) => (
+          <User
+            id={user.id}
+            firstName={user.firstName}
+          />
+        ))}
       </div>
     )
   }
-
-  renderTableData() {
-    return this.state.students.map((student, index) => {
-       const { id, name, age, email } = student //destructuring
-       return (
-          <tr key={id}>
-             <td>{id}</td>
-             <td>{name}</td>
-             <td>{age}</td>
-             <td>{email}</td>
-          </tr>
-       )
-    })
-  }
-
-  renderTableHeader() {
-    let header = Object.keys(this.state.students[0])
-    return header.map((key, index) => {
-      return <th key={index}>{key.toUpperCase()}</th>
-    })
-  }
 }
 
-export default Table;
+// https://itrex-react-lab-files.s3.eu-central-1.amazonaws.com/react-test-api.json
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import Table from './App';
-import Data from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Table />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 ReactDOM.render(
-  <React.StrictMode>
-    <Data />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

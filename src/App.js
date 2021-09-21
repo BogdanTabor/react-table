d73aa40c85f22da090a6c33d2a15c15adcc88fd3
Bogdan Tabor <bogdan.tabor@gmail.com>
2021-09-21 19:28:01 +0300

    restructuring renderTAbleData

--- src/App.js
+++ src/App.js
@@ -26,46 +26,43 @@ class App extends React.Component {
         console.log(this.state.users);
       })
   }
+
+  renderTableData() {
+    return this.state.users.map(user => {
+      const { id, firstName, lastName, email, phone, adress } = user
+      return (
+        <tr key={id}>
+          <td>{id}</td>
+          <td>{firstName}</td>
+          <td>{lastName}</td>
+          <td>{email}</td>
+          <td>{phone}</td>
+          <td>{adress.state}</td>
+        </tr>
+      )
+    })
+  }
   
   render() {
     const { users } = this.state;
 
     return (
-      <div>
-        <table>
-      <caption>Users data</caption>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Email</th>
-          <th>Phone</th>
-          <th>State</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map(user => (
-          <tr key={user.id}>
-            <td>{user.id}</td>
-            <td>{user.firstName}</td>
-            <td>{user.lastName}</td>
-            <td>{user.email}</td>
-            <td>{user.phone}</td>
-            <td>{user.adress.state}</td>
+      <table>
+        <caption>Users data</caption>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>State</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-        <p>List of users</p>
-        <ul>
-          {users.map(user => (
-            <li key={user.id}> {/* TODO: fix "children with the same key" */}
-              {user.firstName}
-            </li>
-          ))}
-        </ul>
-      </div>
+        </thead>
+        <tbody>
+          {this.renderTableData()}
+        </tbody>
+      </table>
     )
   }
 }

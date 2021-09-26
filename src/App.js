c1d8dd52e22e0c96ba0b8eba8b18d61a82615f1d
Bogdan Tabor <bogdan.tabor@gmail.com>
2021-09-26 09:54:55 +0300

    class based version

--- src/App.js
+++ src/App.js
@@ -9,10 +9,6 @@ class App extends React.Component {
       data: [],
       sortKey: null,
       sortDirection: null,
-      sortConfig: {
-        key: null,
-        direction: null,
-      },
     };
   }
 
@@ -52,11 +48,11 @@ class App extends React.Component {
     return (
       <tr>
         <th onClick={() => this.sortableTable('id')}>id</th>
-        <th onClick={() => this.sortableTable('firstName')}>First Name</th>
+        <th onClick={() => this.onSort('firstName')}>First Name</th>
         <th onClick={() => this.requestSort('lastName')}>Last Name</th>
-        <th onClick={() => this.onSort('email')}>Email</th>
-        <th onClick={() => this.onSort('phone')}>Phone</th>
-        <th onClick={() => this.sortableTable('adress.state')}>State</th>
+        <th onClick={() => this.requestSort('email')}>Email</th>
+        <th onClick={() => this.requestSort('phone')}>Phone</th>
+        <th onClick={() => this.requestSort('adress.state')}>State</th>
       </tr>
     )
   }
@@ -65,45 +61,55 @@ class App extends React.Component {
   //https://www.smashingmagazine.com/2020/03/sortable-tables-react/
 
   //<<______________
-  sortableTable(key) {
-    this.setState({sortKey: key})
+  //TODO:
+  //    ? - make sortableTable function able to work with sortDirection
+  //        (it have to sort data using direction and then change it to opposite)
+  // 
+  //    1 - how to set state in correct way ?
+  //    2 - create two separate func: 1) sort data; 2)request sort;
+  //    3 - refactor class to functional component
 
+  sortableTable() { //get sortKey and sortData
     const sortedData = this.state.data;
-    const sortKey = this.state.sortKey;
+    let sortKey = this.state.sortKey;
     let sortDirection = this.state.sortDirection;
 
-    sortedData.sort((a, b) => {
-      if (a[sortKey] < b[sortKey]) {
-        return sortDirection === 'ascending' ? -1 : 1;
-      }
-      if (a[sortKey] > b[sortKey]) {
-        return sortDirection === 'ascending' ? 1 : -1;
-      }
-      return 0;
-    });
+    if (sortKey && sortDirection !== null) {
+      sortedData.sort((a, b) => {
+        if (a[sortKey] < b[sortKey]) {
+          return sortDirection === 'ascending' ? -1 : 1;
+        }
+        if (a[sortKey] > b[sortKey]) {
+          return sortDirection === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
 
-    this.setState({sortDirection: sortDirection})
+    this.setState({sortDirection: sortDirection}) // change it to correct way
+    this.setState({data: sortedData}) // change it to correct way
   }
 
-  requestSort(key) {
-    this.setState({sortKey: key})
-
-    const sortedData = this.state.data;
+  requestSort(key) { //set sortKey and change sortDirection
     const sortKey = this.state.sortKey;
-    let sortDirection = this.state.sortDirection;
+    const sortDirection = this.state.sortDirection;
+    let direction = 'ascending';
 
-    sortDirection = 'ascending';    
     if (sortKey === key && sortDirection === 'ascending') {
-      sortDirection = 'descending';
+      direction = 'descending';
     }
-    this.setState({ sortKey, sortDirection });
+    this.setState({sortDirection: direction}) // change it to correct way
+    this.setState({sortKey: key}) // change it to correct way
+
+    console.log( `requestSort 1: this.key: ${ this.state.sortKey }` );
+    console.log( `requestSort 1: this.direct: ${ this.state.sortDirection }` );
   }
   //____________________________________>>
 
-  //DO NOT WORK ON 'id' and 'adress.state'
-  onSort(sortKey) {
+  //DO NOT WORK WITH 'id' and 'adress.state'
+  onSort(key) {
     const data = this.state.data;
-    data.sort((a, b) => a[sortKey].localeCompare(b[sortKey]))
+    data.sort((a, b) => a[key].localeCompare(b[key]))
     this.setState({data})
   }
    
